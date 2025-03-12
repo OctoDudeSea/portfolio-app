62289a578471611088b3f0ce1de147453df38edd
RobertMS <98914941+OctoDudeSea@users.noreply.github.com>
2025-03-12 12:47:43 +0100

    created the skills transloco json files, modified the skills component to use the translations

--- src/app/information/skills/skills.component.ts
+++ src/app/information/skills/skills.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SkillComponent } from "./skill/skill.component";
 import { TagComponent } from "../../shared/tag/tag.component";
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-skills',
-  imports: [SkillComponent, TagComponent],
+  imports: [SkillComponent, TagComponent, TranslocoDirective],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
