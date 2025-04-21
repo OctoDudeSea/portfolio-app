7be93782d5960df90943ae7f453b929b0e6463a6
RobertMS <98914941+OctoDudeSea@users.noreply.github.com>
2025-04-21 21:53:48 +0200

    implement translations for My Projects section and update related content

--- src/app/information/my-projects/my-projects.component.ts
+++ src/app/information/my-projects/my-projects.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { SkillComponent } from '../skills/skill/skill.component';
 
 @Component({
   selector: 'app-my-projects',
-  imports: [],
+  imports: [ TranslocoDirective, SkillComponent ],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.css'
 })
