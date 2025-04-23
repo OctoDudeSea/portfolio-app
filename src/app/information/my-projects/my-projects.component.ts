c222a0632bb68c2b444b97520b32255876d0a06a
RobertMS <98914941+OctoDudeSea@users.noreply.github.com>
2025-04-23 21:48:52 +0200

    add tags to My Projects section and update project descriptions in English and Spanish

--- src/app/information/my-projects/my-projects.component.ts
+++ src/app/information/my-projects/my-projects.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { SkillComponent } from '../skills/skill/skill.component';
+import { TagComponent } from '../../shared/tag/tag.component';
 
 @Component({
   selector: 'app-my-projects',
-  imports: [ TranslocoDirective, SkillComponent ],
+  imports: [ TranslocoDirective, SkillComponent, TagComponent ],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.css'
 })
