1ccc5c445d050192216eeaf9205638c86d076a2e
RobertMS <98914941+OctoDudeSea@users.noreply.github.com>
2025-04-24 10:09:11 +0200

    implement education section translations and update related content

--- src/app/information/education/education.component.ts
+++ src/app/information/education/education.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { SkillComponent } from '../skills/skill/skill.component';
+import { TagComponent } from '../../shared/tag/tag.component';
 
 @Component({
   selector: 'app-education',
-  imports: [],
+  imports: [ TranslocoDirective, SkillComponent, TagComponent ],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
