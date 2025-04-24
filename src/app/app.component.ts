600af4055f30d199207da2dd4456c6e5a2678bd4
RobertMS <98914941+OctoDudeSea@users.noreply.github.com>
2025-04-24 11:22:23 +0200

    add responsive design improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, computed, input } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { InformationComponent } from './information/information.component';
+import { LangSwitchComponent } from "./lang-switch/lang-switch.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ NavbarComponent, InformationComponent ],
+  imports: [ NavbarComponent, InformationComponent, LangSwitchComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
